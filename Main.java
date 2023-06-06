bc2ab23cb70642c7e26c88a79558c7d9bfd352be
Vivek Vardhan <91594529+vivekvardhan2810@users.noreply.github.com>
2023-06-06 22:25:29 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 //Java Practice//
 /* The code below will print the words u*/
+
 class Main{
   public static void main(String args[]){
    System.out.println("Hello world");
@@ -7,9 +8,16 @@ class Main{
    System.out.println("It is Awesome");
    System.out.println("I will print on the same line.");
    System.out.println("Myself Vivek Vardhan and learning java Slowly");
+    
 /* The Below input shows use the println() method to print numbers
 However, unlike text, we don't put numbers inside double quotes*/ 
    System.out.println(3);
    System.out.println(3+6);
    System.out.println(2*10);
    System.out.println("Myself Vivek Vardhan and learning java Slowly");
+    
+/* Java Vriables like String (stores text, such as "Hello"), float (decimals), int such as (1,2 or any number), boolean (T/F), char (stores single characters, such as 'a' or 'B')*/
+
+    String name = "Vivek Vardhan";
+    System.out.println(name);
+    
