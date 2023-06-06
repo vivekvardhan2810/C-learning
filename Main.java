0219a3a3ee15c4581073aa6ed9649f644a7eb277
Vivek Vardhan <91594529+vivekvardhan2810@users.noreply.github.com>
2023-06-06 21:36:18 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,12 @@
+//Java Practice//
+/* The code below will print the words*/
+class Main{
+  public static void main(String args[]){
+   System.out.println("Hello world");
+   System.out.println("I am learning Java");
+   System.out.println("It is Awesome");
+   System.out.println("I will print on the same line.");
+   System.out.println(3);
+   System.out.println(3+6);
+   System.out.println(2*10);
+   System.out.println("Myself Vivek Vardhan and learning java Slowly");
