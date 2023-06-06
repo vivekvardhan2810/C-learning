ed868ac4d299d114fe2c15af0929ea256453e53b
Vivek Vardhan <91594529+vivekvardhan2810@users.noreply.github.com>
2023-06-06 22:32:44 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,23 +1,35 @@
 //Java Practice//
-/* The code below will print the words u*/
-
-class Main{
-  public static void main(String args[]){
-   System.out.println("Hello world");
-   System.out.println("I am learning Java");
-   System.out.println("It is Awesome");
-   System.out.println("I will print on the same line.");
-   System.out.println("Myself Vivek Vardhan and learning java Slowly");
-    
-/* The Below input shows use the println() method to print numbers
-However, unlike text, we don't put numbers inside double quotes*/ 
-   System.out.println(3);
-   System.out.println(3+6);
-   System.out.println(2*10);
-   System.out.println("Myself Vivek Vardhan and learning java Slowly");
-    
-/* Java Vriables like String (stores text, such as "Hello"), float (decimals), int such as (1,2 or any number), boolean (T/F), char (stores single characters, such as 'a' or 'B')*/
-
-    String name = "Vivek Vardhan";
+/* The code below will print the words*/
+class Main {
+  public static void main(String args[]) {
+    System.out.println("Hello world");
+    System.out.println("I am learning Java");
+    System.out.println("It is Awesome");
+    System.out.println("I will print on the same line.");
+    System.out.println("Myself Vivek Vardhan and learning java Slowly");
+    /*
+     * The Below input shows use the println() method to print numbers
+     * However, unlike text, we don't put numbers inside double quotes
+     */
+    System.out.println(3);
+    System.out.println(3 + 6);
+    System.out.println(2 * 10);
+    /*
+     * Java Variables like String (stores text, such as "Hello"), float (decimals),
+     * int such as (1,2 or any number), boolean (T/F), char (stores single
+     * characters, such as 'a' or 'B')
+     */
+    String name = "John";
     System.out.println(name);
+
+    int myNum = 15;
+    myNum = 20;
+    System.out.println(myNum);
+
+    int x = 5;
+    int y = 7;
+    // int z = x + y;
+    System.out.println(x + y);
+  }
+}
     
