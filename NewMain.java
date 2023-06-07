d00cb33d62e391f9a4bf37f5af15d37eb239c4c1
Vivek Vardhan <91594529+vivekvardhan2810@users.noreply.github.com>
2023-06-08 00:01:32 +0530

    Update NewMain.java

--- NewMain.java
+++ NewMain.java
@@ -1,5 +1,5 @@
 //Java Practice ( The below Content Written in Comment taken from W3 Schools For reference)//
-// From This File i Will learn about Conditions and Statements, Switch, Loops, Break and Arrays//
+// From This File I Will learn about Conditions and Statements, Such as if, else, else if, Switch, Loops, Break and Arrays//
 class NewMain {
    public static void main(String args[]) {
       
