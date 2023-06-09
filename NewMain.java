5d25fa4609f9e495c768b2d118e94a2e68b4aa1d
Vivek Vardhan <91594529+vivekvardhan2810@users.noreply.github.com>
2023-06-09 21:54:44 +0530

    Update NewMain.java

--- NewMain.java
+++ NewMain.java
@@ -2,6 +2,6 @@
 /* From This File I Will learn about Conditions and Statements, Such as if, else, else if, Switch, Loops, Break and Arrays*/
 class NewMain {
    public static void main(String args[]) {
-      
+      /*Java Conditions and If Statements , Java supports the usual logical conditions from mathematics*/
    }
 }
