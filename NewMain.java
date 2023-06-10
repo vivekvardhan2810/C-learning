2497f160ff8e81712b223a8b126d45b0b5164880
Vivek Vardhan <91594529+vivekvardhan2810@users.noreply.github.com>
2023-06-10 20:06:42 +0530

    Update NewMain.java

--- NewMain.java
+++ NewMain.java
@@ -62,7 +62,30 @@ class NewMain {
     The break and default keywords are optional, and will be described later in this chapter*/
 
     //Examples//
-
+    int day = 5;
+    switch (day) {
+      case 1:
+        System.out.println("Monday");
+        break;
+      case 2:
+        System.out.println("tuesday");
+        break;
+      case 3:
+        System.out.println("Wedneday");
+        break;
+      case 4:
+        System.out.println("Thursday");
+        break;
+      case 5:
+        System.out.println("Friday");
+        break;
+      case 6:
+        System.out.println("Saturday");
+        break;
+      case 7:
+        System.out.println("Sunday");
+        break;
+    }
 
   }
 }  
