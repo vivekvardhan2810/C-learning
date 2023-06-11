70b2368e0017d491136e76b0e1ad08a04f6d01ea
Vivek Vardhan <91594529+vivekvardhan2810@users.noreply.github.com>
2023-06-11 10:44:45 +0530

    Update NewMain.java

--- NewMain.java
+++ NewMain.java
@@ -124,6 +124,9 @@ class NewMain {
    code block to be executed
 
     } */
+/* Statement 1 Statement 1 is executed (one time) before the execution of the code block
+  Statement 2 defines the condition for executing the code block
+  Statement 3 is executed (every time) after the code block has been executed*/
 
   //Examples//
    for (int i = 0; i < 5; i++) {
