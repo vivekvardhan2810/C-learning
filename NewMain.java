324cbe477c989b177e9df329d6c410312ac55e18
Vivek Vardhan <91594529+vivekvardhan2810@users.noreply.github.com>
2023-06-11 11:07:53 +0530

    Update NewMain.java

--- NewMain.java
+++ NewMain.java
@@ -151,6 +151,13 @@ class NewMain {
 
 The "inner loop" will be executed one time for each iteration of the "outer loop"*/
 
-
+ //Outer loop//
+      for (int i = 1; i<=2; i++) {
+        System.out.println("Outer: " + i);
+      }
+    //Inner loop//
+      for (int j = 1; j<=3; j++) {
+        System.out.println("Inner " + j);
+      }
   }
 }  
