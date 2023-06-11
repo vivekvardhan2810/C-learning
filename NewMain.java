74ea3a58ef95f9ae0095fae0a486862d62b63b46
Vivek Vardhan <91594529+vivekvardhan2810@users.noreply.github.com>
2023-06-11 10:49:34 +0530

    Update NewMain.java

--- NewMain.java
+++ NewMain.java
@@ -133,6 +133,12 @@ class NewMain {
    for (int i = 0; i < 5; i++) {
         System.out.println(i);
       }
+  /*The Above Example explained 
+    Statement 1 sets a variable before the loop starts (int i = 0).
+
+    Statement 2 defines the condition for the loop to run (i must be less than 5). If the condition is true, the loop will start over again, if it is false, the loop will end.
+
+    Statement 3 increases a value (i++) each time the code block in the loop has been executed.*/
 
 
   }
