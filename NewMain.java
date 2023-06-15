319a62ff54853f91de3250d0e2b4857d7846413e
Vivek Vardhan <91594529+vivekvardhan2810@users.noreply.github.com>
2023-06-15 11:48:15 +0530

    Update NewMain.java

--- NewMain.java
+++ NewMain.java
@@ -262,6 +262,12 @@ int[][] myNumbers = { {1, 2, 3, 4}, {5, 6, 7} };
       }
     }
 
-     
+  /*Java Methods
+   A method is a block of code which only runs when it is called.
+   You can pass data, known as parameters, into a method.
+   Methods are used to perform certain actions, and they are also known as functions.
+   Why use methods? To reuse code: define the code once, and use it many times.*/
+
+   
   }
 }  
