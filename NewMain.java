37f520d94422ed099a9f7d841de9bf0e7a22824a
Vivek Vardhan <91594529+vivekvardhan2810@users.noreply.github.com>
2023-06-15 22:57:46 +0530

    Update NewMain.java

--- NewMain.java
+++ NewMain.java
@@ -301,6 +301,7 @@ public static void main(String[] args) {
     }
 
    //Return a value//
+  /* The void keyword, used in the examples above, indicates that the method should not return a value. If you want the method to return a value, you can use a primitive data type (such as int, char, etc.) instead of void, and use the return keyword inside the method*/
 
   }
 }  
