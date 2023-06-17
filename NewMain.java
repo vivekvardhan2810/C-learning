85a32a0fa4ddbc8374034ba64fc730eab740faab
Vivek Vardhan <91594529+vivekvardhan2810@users.noreply.github.com>
2023-06-17 09:49:45 +0530

    Update NewMain.java

--- NewMain.java
+++ NewMain.java
@@ -338,6 +338,21 @@ Static int myMethod(int x) {
 //Java Recursion//
   /* Recursion is the technique of making a function call itself. This technique provides a way to break complicated problems down into simple problems which are easier to solve*/
 
+public class Example1 {
+  public static void main(String[] args) {
+    int result = sum(9);
+    System.out.println(result);
+  }
+  public static int sum(int k) {
+    if (k > 0) {
+      return k + sum(k - 1);
+    } else {
+      return 0;
+     }
+
+
+  }
+}
 
   }
 }  
