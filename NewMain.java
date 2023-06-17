ea13c9afd51e610632b6a66d6e798244f67666bd
Vivek Vardhan <91594529+vivekvardhan2810@users.noreply.github.com>
2023-06-17 10:37:02 +0530

    Update NewMain.java

--- NewMain.java
+++ NewMain.java
@@ -357,6 +357,18 @@ public class Example1 {
 
 //Halting Condition//
 // Use recursion to add all of the numbers between 5 to 10//
-
+public class Main {
+  public static void main(String[] args) {
+    int result = sum(5, 10);
+    System.out.println(result);
+  }
+  public static int sum(int start, int end) {
+    if (end > start) {
+      return end + sum(start, end - 1);
+    } else {
+      return end;
+    }
+  }
+}
   }
 }  
