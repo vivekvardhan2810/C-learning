aceed6ede54c21aec8ff74bf5a3d19ed11bf9936
Vivek Vardhan <91594529+vivekvardhan2810@users.noreply.github.com>
2023-06-21 17:24:03 +0530

    Update Example1.java

--- Example1.java
+++ Example1.java
@@ -1,1 +1,32 @@
-
+// Java implementation to find unique digit
+// numbers in a range (Input  like 10 to 15)
+//This Solution is taken just for practice not to copy it 
+public class Example1
+{
+	static void printUnique(int l, int r)
+	{
+		for (int i=l ; i<=r ; i++)
+		{
+			int num = i;
+			boolean visited[] = new boolean[10];
+	
+			while (num != 0)
+			{
+				if (visited[num % 10])
+					break;
+	
+				visited[num%10] = true;
+	
+				num = num/10;
+			}
+			if (num == 0)
+				System.out.print(i + " ");
+		}
+	}
+	
+	public static void main(String args[])
+	{
+		int l = 10, r = 15;
+		printUnique(l, r);
+	}
+}
