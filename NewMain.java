db911161a9b3d2e7eab48e79a9ea2e3b2eb8cf4e
Vivek Vardhan <91594529+vivekvardhan2810@users.noreply.github.com>
2023-06-21 16:54:36 +0530

    Update NewMain.java

--- NewMain.java
+++ NewMain.java
@@ -455,6 +455,10 @@ public class NewMain {
 
 //Java Encapsulation//
 //Get and Set//
-      
+/*The meaning of Encapsulation, is to make sure that "sensitive" data is hidden from users. To achieve this, you must:
+
+declare class variables/attributes as private
+provide public get and set methods to access and update the value of a private variable*/
+ 
   }
 }  
