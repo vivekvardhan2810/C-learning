4c2d16468ff09537c8747af4c59b9f50d52ff176
Vivek Vardhan <91594529+vivekvardhan2810@users.noreply.github.com>
2023-06-22 10:54:52 +0530

    Update NewMain.java

--- NewMain.java
+++ NewMain.java
@@ -478,6 +478,23 @@ class NewMain {
   }
 }
 
+class Example1 {
+	protected String brand = "Ford";
+	public void honk() {
+		System.out.println("Tuut, tuut!");
+	}
+}
+
+class car extends Example1 {
+	private String modelName = "Mustang";
+	public static void main(String[] args) {
+		car myCar  = new car();
+		myCar.honk();
+
+		System.out.println(myCar.brand + " " + myCar.modelName);
+	}
+}
+   
 
   }
 }  
